38a6d94b8fdd67b42423541fe1a80ee7518ddd9e
ciripel <nedelcu.alexandru@yahoo.com>
2018-11-11 13:19:09 +0200

    v.1.0 - externalized except_roles, banned_words

--- bot.py
+++ bot.py
@@ -31,19 +31,21 @@ async def on_message(msg):
         return
 
     # Bot ignore messages from special roles
-    if (
-        "Core-Team" in [role.name for role in msg.author.roles]
-        or "Support-Team" in [role.name for role in msg.author.roles]
-        or "Contributors" in [role.name for role in msg.author.roles]
-        or "Team" in [role.name for role in msg.author.roles]
-    ):
-        return
-
-    for link in data["links"]:
-        if link in msg.content:
+    for ex_rol in data["except_roles"]:
+        if ex_rol in [role.name for role in msg.author.roles]:
             return
-    if "https://discordapp.com/invite/" in msg.content:
-        await client.delete_message(msg)
+    # BOT is ignoring exceptions
+    # This is usefull if the exception is containing the forbidden word (expresion)
+    for ex_link in data["except_links"]:
+        if ex_link in msg.content:
+            return
+    # Bot is parsing all the words (expresions) list and check if the message is
+    # containing any of them and delete the messages that contain them
+    for ban_word in data["banned_words"]:
+        if ban_word in msg.content:
+            await client.delete_message(msg)
+            # return is used here to not have double words and call a delete_message(deleted_message)
+            return
 
 
 @client.event
